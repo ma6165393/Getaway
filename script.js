26aef93693d38030ec1a1726aa34fbc01b3c4b47
ma6165393 <129995662+ma6165393@users.noreply.github.com>
2026-06-28 03:13:48 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", () => {
+    window.location.href = "https://www.amazon.com/?_encoding=UTF8&tag=mntzr-20&linkCode=ur2&linkId=fbdfb9b1ea167046640ea9a6800add6e&camp=1789&creative=9325&subId=M7656225529548243020";
+});
