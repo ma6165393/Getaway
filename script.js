8c8638c6a18fa3f2e56736c14d588ee02b9d0c30
ma6165393 <129995662+ma6165393@users.noreply.github.com>
2026-06-28 15:12:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const btn = document.getElementById("btn");
 
 btn.addEventListener("click", () => {
-    window.location.href = "https://www.amazon.com/?_encoding=UTF8&tag=mntzr-20&linkCode=ur2&linkId=fbdfb9b1ea167046640ea9a6800add6e&camp=1789&creative=9325&subId=M7656225529548243020";
+    window.location.href = "https://de6jvomfbm0af.cloudfront.net/public/locker.php?it=948518&key=f6814";
 });
